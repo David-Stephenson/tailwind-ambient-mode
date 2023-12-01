5f2bc77fbddc743d097f6f1e9f2ec19db7d9c7f8
David Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2023-12-01 09:39:12 -0500

    Code

---
+++ README.md
@@ -0,0 +1,32 @@
+# Ambient Mode Example
+
+This project showcases a minimalist video background glow, inspired by YouTube's ambient mode. It's designed using Tailwind CSS and built on Sveltekit, although Sveltekit is not a prerequisite.
+
+![Demo Image](images/example.png)
+
+## Prerequisites
+
+Ensure you have the latest version of npm installed.
+
+## Installation & Setup
+
+1. Clone the repository to your local machine.
+2. Navigate to the project directory.
+3. Install the required dependencies:
+
+```bash
+npm i
+```
+
+## Development
+
+Start the development server with:
+
+```bash
+npm run dev
+```
+
+## Acknowledgments
+
+- Inspired by YouTube's ambient mode
+- Built with Tailwind CSS and Sveltekit
---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let video; // <video> element
+	let canvas; // <canvas> element
+
+	onMount(() => {
+		const ctx = canvas.getContext('2d');
+
+		// Draw the video to the canvas every 50ms
+		video.addEventListener(
+			'play',
+			function () {
+				const draw = function () {
+					if (video.paused || video.ended) return;
+					ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+					setTimeout(draw, 50);
+				};
+				draw();
+			},
+			false
+		);
+	});
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen relative overflow-hidden">
+	<!-- Background glow canvas -->
+	<canvas bind:this={canvas} class="aspect-video h-[34rem] absolute blur-[100px]" aria-hidden="true"
+	></canvas>
+
+	<!-- 
+        Le Tailwind definitions:
+        aspect-video: 16:9 aspect ratio
+        h-[34rem]: 34rem height
+        absolute: position absolute to parent
+        blur-[100px]: 100px blur
+        aria-hidden="true": hide from screen readers
+     -->
+
+	<!-- Video element -->
+	<video
+		bind:this={video}
+		class="aspect-video h-[30rem] relative border-4 border-white rounded-md"
+		src="https://data.davidstephenson.net/Hero/vein-1280x720/video.m4v"
+		controls
+		muted
+		loop
+	></video>
+
+	<!-- 
+        Le Tailwind definitions:
+        aspect-video: 16:9 aspect ratio
+        h-[30rem]: 30rem height
+        relative: position relative to parent
+        border-4: 4px border
+        border-white: white border
+        rounded-md: medium rounded corners
+     -->
+
+	<div class="fixed bottom-0 right-0">
+		<p class="text-white text-right">Video: Beeple - GLASS VEIN</p>
+	</div>
+</div>
